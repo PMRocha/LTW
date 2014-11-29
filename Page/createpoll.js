68f8bde864d9008950e31201dedcf5c48487607a
PMRocha <ei11078@fe.up.pt>
2014-11-29 14:33:22 +0000

    create poll with one question and variable answers

--- Page/createpoll.js
+++ Page/createpoll.js
@@ -1,15 +1,16 @@
 $( document ).ready(function() {
  	
-	var max_fields      = 6; //maximum input boxes allowed
+	var max_questions   = 10;
+	var max_answers     = 6;
     var wrapper         = $(".input_fields_wrap"); //Fields wrapper
     var add_button      = $("#input_button"); //Add button ID
     
     var x = 2; //initlal text box count
     $(add_button).click(function(e){ //on add input button click
         e.preventDefault();
-        if(x < max_fields){ //max input box allowed
+        if(x < max_answers){ //max input box allowed
             x++; //text box increment
-            $(wrapper).append('<div><br><input type="text" name="answer"/><br><a href="#" class="remove_field">Remove</a></div>'); //add input box
+            $(wrapper).append('<div><p>Answer '+ x +':</p><input type="text" name="answer'+ x +'"/><br><a href="#" class="remove_field">Remove</a></div>'); //add input box
         }
     });
     
@@ -27,12 +28,6 @@ function firstTagChild(element) {
 }
 
 function addQuestion() {
-	console.log("you just clicked a thing");
 	var original = firstTagChild($("#poll"));
 	original.clone().appendTo($("#questionsAdded"));
-}
-
-function addAnswer() {
-	console.log("you just clicked a thing");
-	$("#answerChunk").clone().appendTo($("#answersAdded"));
 }
--- Page/createpoll.php
+++ Page/createpoll.php
@@ -1,54 +1,53 @@
 <?php
 session_start();
 	if(isset($_SESSION['name']) && $_SESSION['name'] != '') {
+	
 		$db = new PDO('sqlite:../DataBase/Poll.db');
-
 		$db->beginTransaction();
-		//store poll
-		$stmt = $db->prepare('INSERT INTO Poll(CreatorID) VALUES (?)');
-		$stmt->execute(array($_SESSION['userID']));
-		//store question poll relation
-		$stmt = $db->prepare('INSERT INTO Question(QuestionText,QuestionPoll)
+	
+		$cont = 0;
+		foreach ($_POST as $i => $value) {
+			if($cont >0){
+				echo("Answer:".$_POST[$i]."\n");
+				$stmt = $db->prepare('INSERT INTO Answer(AnswerOption,AnswerToQuestion)
+							SELECT  ?, QuestionID
+							FROM    Question 
+							ORDER BY QuestionID DESC LIMIT 1');
+				$stmt->execute(array($_POST[$i]));
+				
+				$stmt = $db->prepare('INSERT INTO AnswerResult(AnswerResults,ResultCont)
+							SELECT   AnswerID, 0
+							FROM    Answer 
+							ORDER BY AnswerID DESC LIMIT 1');
+				$stmt->execute();
+			}
+			else{
+				echo("Question:".$_POST[$i]."\n");
+				$stmt = $db->prepare('INSERT INTO Poll(CreatorID) VALUES (?)');
+				$stmt->execute(array($_SESSION['userID']));
+				//store question poll relation
+				$stmt = $db->prepare('INSERT INTO Question(QuestionText,QuestionPoll)
 							SELECT  ?, PollID
 							FROM    Poll 
 							ORDER BY PollID DESC LIMIT 1');
-		$stmt->execute(array($_POST['question']));
-		//store answer 1
-		$stmt = $db->prepare('INSERT INTO Answer(AnswerOption,AnswerToQuestion)
-							SELECT  ?, QuestionID
-							FROM    Question 
-							ORDER BY QuestionID DESC LIMIT 1');
-		$stmt->execute(array($_POST['answer1']));
-		//store answer 1?
-		$stmt = $db->prepare('INSERT INTO AnswerResult(AnswerResults,ResultCont)
-							SELECT   AnswerID, 0
-							FROM    Answer 
-							ORDER BY AnswerID DESC LIMIT 1');
-		$stmt->execute();
-		//store answer 2
-		$stmt = $db->prepare('INSERT INTO Answer(AnswerOption,AnswerToQuestion)
-							SELECT  ?, QuestionID
-							FROM    Question 
-							ORDER BY QuestionID DESC LIMIT 1');
-		$stmt->execute(array($_POST['answer2']));
-		//store answer 2?
-		$stmt = $db->prepare('INSERT INTO AnswerResult(AnswerResults,ResultCont)
-							SELECT   AnswerID, 0
-							FROM    Answer 
-							ORDER BY AnswerID DESC LIMIT 1');
-		$stmt->execute();
+				$stmt->execute(array($_POST[$i]));
+			}
+			$cont++;
+		}
 
 		$db->commit();
 	}
 
 	$db = new PDO('sqlite:../DataBase/Poll.db');
 
-	//delete everything
-	$stmt = $db->prepare('DELETE FROM Poll');
-	$stmt->execute();
 	$stmt = $db->prepare('SELECT * FROM Poll');
 	$stmt->execute();
 	while($row = $stmt->fetch()) {
 		echo $row['CreatorID'] . "\n";
+	}
+		$stmt = $db->prepare('SELECT * FROM Question');
+	$stmt->execute();
+	while($row = $stmt->fetch()) {
+		echo $row['QuestionText'] . "\n";
 	}
 ?>
