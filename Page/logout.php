2057a1ff4cf15eac2dfc9aba3a60d6e7a45147ec
zephiro309 <zephiro309@gmail.com>
2014-12-01 22:49:49 +0000

    Added style to logout page.

--- Page/logout.php
+++ Page/logout.php
@@ -5,6 +5,7 @@ session_start();
 		session_destroy();
 		echo "Logged out! Redirecting...";
 		?>
+		<link rel="stylesheet" type="text/css" href="style.css">
 		<meta http-equiv="refresh" content="3;url=starthere.html"/>
 		<?php
 	}
