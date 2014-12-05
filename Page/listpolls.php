cb92631961bc03cfe61a102578a43baf1b44240d
PMRocha <ihastealth@gmail.com>
2014-12-05 12:08:19 +0000

    maybe final css

--- Page/listpolls.php
+++ Page/listpolls.php
@@ -4,11 +4,29 @@
 
 	$stmt->execute();
 	?>
-	<h3>Available polls:</h3>
+	<head>
+		<link rel="stylesheet" type="text/css" href="style.css">
+		<title>List Polls - POLL RAVAGER 3000</title>
+	</head>
+	<div id="wrap">
+		<div id="header">
+			<h1>POLL RAVAGER 3000</h1>
+			<br>
+			<h2>Available polls:</h2> 
+	</div>
+	<div class="centered">
 	<?php
 	while($row = $stmt->fetch()) {
 		?>
-			<a href="poll.php?pollID=<?=$row['QuestionPoll']?>"><?=$row['QuestionText']?></a><p></p>
+			<a href="poll.php?pollID=<?=$row['QuestionPoll']?>"><h2> -> <?=$row['QuestionText']?></h2></a><p></p>
+			
 		<?php
 	}
 ?>
+	</div>
+	<div style="clear: both;"> </div>
+	<div id="footer">
+		<p>LTW FEUP 2014/2015</p>
+		<p>Miguel Mendes, Pedro Rocha</p>
+	</div>
+</div>
